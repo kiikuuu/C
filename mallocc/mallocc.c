ba30e5dab4fdb77488078fbebeb13fa8089faafb
kiikuuu <izaccknd@hotmail.com>
2017-09-10 01:04:49 -0300

    Update mallocc.c

--- mallocc/mallocc.c
+++ mallocc/mallocc.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void* mallocc(size_t size){
+
+	void* value = malloc(size);
+	if(value == NULL){
+		fprintf(stderr, "Não foi possivel alocar memoria!\n");
+		exit(1);
+	}else
+		return value;
+}
+
+void* callocc(size_t nItens , size_t size){
+
+	void* value = calloc(nItens , size);
+	if(value == NULL){
+		fprintf(stderr,"Não foi possivel alocar memoria!\n");
+		exit(1);
+	}else
+		return value;
+}
