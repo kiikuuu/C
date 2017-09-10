87df493673a461fcace9dd6cea78a706d3eb4725
kiikuuu <izaccknd@hotmail.com>
2017-09-10 01:02:06 -0300

    Update matrix.h

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,1 +1,12 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
 
+#include <stdbool.h>
+#include "/home/izac/Documents/C/implementacoes/mallocc/mallocc.h"
+
+double** initMatriz( const size_t n);
+bool setMatrizTriangular(double** m, const size_t n , int* fator );
+double determinante(double** m, bool hasDet , const size_t n , const int fator);
+bool seLinhaColunaNulo( double** m , const size_t n );
+
+#endif
